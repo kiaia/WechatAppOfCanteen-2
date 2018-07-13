2102b72b3e9aa34b4c4c16a5262f12b898340ab5
Zx55 <czr.cn.525@gmail.com>
2018-07-13 16:41:03 +0800

    demo

---
+++ pages/details/details.js
@@ -0,0 +1,54 @@
+/* /pages/details/details.js */
+/*关闭抽屉菜单时太快*/
+Page({
+  data: {
+    enableScrollY: true,
+    showModalStatus: false
+  },
+  powerDrawer: function (e) {
+    var currentStatu = e.currentTarget.dataset.statu;
+    this.util(currentStatu)
+  },
+  util: function (currentStatu) {
+    var animation = wx.createAnimation({
+      duration: 600,
+      timingFunction: 'ease',
+      delay: 0
+    });
+
+    this.animation = animation;
+
+    animation.translateY(800).step();
+
+    this.setData({
+      animationData: animation.export()
+    })
+
+    setTimeout(function () {
+      animation.translateY(0).step()
+      this.setData({
+        animationData: animation
+      })
+
+      if (currentStatu == "close") {
+        //console.log(this.enableScrollY)
+        this.setData(
+          {
+            showModalStatus: false,
+            enableScrollY: true
+          }
+        );
+      }
+    }.bind(this), 100)
+
+    if (currentStatu == "open") {
+      console.log("open")
+      this.setData(
+        {
+          showModalStatus: true,
+          enableScrollY: false
+        }
+      );
+    }
+  }
+})  
---
+++ pages/order/order.js
@@ -0,0 +1,36 @@
+Page({
+  data: {
+    toView: 'list1',
+    scrollTop: 100,
+    maginleft: '10%',
+    src: "/images/446105925964471455.jpg",
+    state: "未完成",
+    orderlist: [0, 1, 2, 3, 4, 5],
+    ordername: '西红柿炒番茄',
+    ordertime: '5月19日 23:59'
+  },
+  btn1: function () {
+    this.setData({
+      state: '未完成'
+    })
+  },
+  btn2: function () {
+    this.setData({
+      state: '未评论'
+    })
+  },
+  btn3: function () {
+    this.setData({
+      state: '已完成'
+    })
+  },
+  upper: function (e) {
+    console.log(e)
+  },
+  lower: function (e) {
+    console.log(e)
+  },
+  scroll: function (e) {
+    console.log(e)
+  },
+})
